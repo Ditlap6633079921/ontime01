b931e5aa442a056e7f5e2196a79102bccb207ad5
chwlm <107909884+chwlm@users.noreply.github.com>
2024-05-05 17:02:25 +0700

    Clear code

--- src/pane/HomePane.java
+++ src/pane/HomePane.java
@@ -1,7 +1,6 @@
 package pane;
 
 import javafx.scene.effect.DropShadow;
-import javafx.scene.input.MouseDragEvent;
 import javafx.scene.layout.Background;
 import javafx.scene.layout.BackgroundFill;
 import javafx.scene.layout.CornerRadii;
--- src/pane/RootPane.java
+++ src/pane/RootPane.java
@@ -1,16 +1,13 @@
 package pane;
 
-import utils.Goto;
-import javafx.geometry.Pos;
 import javafx.geometry.Insets;
-import javafx.scene.text.Font;
-import javafx.scene.text.Text;
+import javafx.geometry.Pos;
+import javafx.scene.layout.Background;
+import javafx.scene.layout.BackgroundFill;
+import javafx.scene.layout.CornerRadii;
 import javafx.scene.layout.VBox;
 import javafx.scene.paint.Color;
-import javafx.scene.text.FontWeight;
-import javafx.scene.layout.Background;
-import javafx.scene.layout.CornerRadii;
-import javafx.scene.layout.BackgroundFill;
+import utils.Goto;
 
 //This class for layout behind of all page
 public class RootPane extends VBox {
